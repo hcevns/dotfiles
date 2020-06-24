b883b60aac9147425c4be2adbf13b2e3c18bdbcc
Harry Evans <hcevns@gmail.com>
2020-06-24 01:12:31 +0100

    Set up Fish symlinks

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 . ~/.config/fish/aliases.fish
 
 if not functions -q fisher
-  set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-  curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
   fish -c fisher
 end
--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 
-# Add submodules (e.g. zsh plugins)
-(cd ~/.dotfiles && git submodule init && git submodule update)
-
 # Remove existing config files
-rm ~/.editorconfig
+
+rm ~/.bashrc
+rm -rf ~/.config/fish
 rm ~/.gitconfig
-rm ~/.gitignore
-rm ~/.prettierrc
-rm -rf ~/.zsh
+rm ~/.gitignore_global
 rm ~/.zshrc
 
 # Symlink dotfiles
-ln -s ~/.dotfiles/.editorconfig ~/.editorconfig
+
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+mkdir ~/.config/fish
+ln -s ~/.dotfiles/fish/aliases.fish ~/.config/fish/aliases.fish
+ln -s ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/.dotfiles/fish/fishfile ~/.config/fish/fishfile
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/git/.gitignore ~/.gitignore
-ln -s ~/.dotfiles/.prettierrc ~/.prettierrc
-ln -s ~/.dotfiles/zsh/.zsh ~/.zsh
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
