cc64f641e3518249e7629da3746d56ea18ff463d
Harry Evans <hcevns@gmail.com>
2020-07-09 20:20:34 +0100

    Add Karabiner config

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ rm ~/.bashrc
 rm -rf ~/.config/fish
 rm ~/.gitconfig
 rm ~/.gitignore_global
+rm -rf ~/.config/karabiner
 rm ~/.zshrc
 
 # Symlink dotfiles
@@ -17,4 +18,6 @@ ln -s ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/.dotfiles/fish/fishfile ~/.config/fish/fishfile
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+mkdir ~/.config/karabiner
+ln -s ~/.dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
