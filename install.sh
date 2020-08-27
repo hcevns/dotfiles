2b19e6e118b4ffd8ca121dd2fa9618e6bf76054b
Harry Evans <hcevns@gmail.com>
2020-08-27 19:48:24 +0100

    Create .config if it doesn't exist

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ rm ~/.zshrc
 # Symlink dotfiles
 
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
-mkdir ~/.config/fish
+mkdir -p ~/.config/fish
 ln -s ~/.dotfiles/fish/aliases.fish ~/.config/fish/aliases.fish
 ln -s ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/.dotfiles/fish/fishfile ~/.config/fish/fishfile
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
-mkdir ~/.config/karabiner
+mkdir -p ~/.config/karabiner
 ln -s ~/.dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
